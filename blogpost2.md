0770c6698313d5ded298778d0768c6b656d282a6
Kira <apple@KiradeMacBook-Pro.local>
2018-01-30 16:14:42 -0500

    add blog post & change codes

---
+++ blogpost2.md
@@ -0,0 +1,10 @@
+# This is my Blog Post
+------
+
+As a coding beginner, I found P5.js is a relatively easy-to-use javascript library. I have learnt coding in swift language before, so I would say I have some basic understandings of programming. But I am still very excited to learn Javascript and how to creates graphics by writing codes. 
+
+One of the exciting features that P5.js has is drawing shapes. I think this feature would provide me with multiple opportunities to create arts and design interactive graphics by using codes. Another thing that I feel excited about is that P5.js is based on web, so every drawing that we have done by writing codes could be executed immediately on the web, and we can use lots of functions to make our designs interactive, like scrolls, sliders, and clicks. 
+
+In the first two weeks, I recapped my programming knowledge by going back to the notes for one class that I have taken in my junior year, “fundamental programming in swift language”. But unfortunately I have to admit I have forgotten many basic knowledge of programming, such as what is a "Class"/"Library”/"Array"/"Dictionary"/. I hope that we can get to these points sometimes. And I also wonder what exactly can the P5.js library do and how to import more libraries into my sketch and how to use them accordingly. 
+
+ 
--- sketch.js
+++ sketch.js
@@ -1,48 +1,101 @@
 var r,g,b;
-var x,y;
+//var x,y;
+var peach;
+var peaches = [];
+var peaches2 = [];
+//var x1,y1;
+
+
+function preload(){
+    peach = loadImage('image/peachemoji.png');
+    
+}
+
  
 function setup(){
     createCanvas(1200, 1200);
-    r = random(255);
-    g = random (255);
-    b = random (255);
-    x = width/ 2;
-    y = height;
-    
-    
-   
+    r = random(0, 255);
+    g = random(0, 255);
+    b = random(0, 255);
+    //x = width/ 2;
+    //y = height;  
+    //x1 = width/3;
+    //y1 = height;
+    for (var i = 0; i<30 ; i++){
+        peaches[i] = new Peach(i*2+200,height,50,50,random(1,10));
 
+    }
+    for (var e = 0;e<30;e++){
+        peaches2[e] = new Peach(1200-(e*2+200),height,50,50,random(1,10));
+
+    }  
 }
+
 function draw(){
     background(255, 204, 0);
     stroke(r, g, b);
-    fill(r, g, b,255);
+    fill(r, g, b);
     ellipse(600, 600, 400, 400);
     stroke(50);
     fill(125);
-    ellipse(x,y,50,50)
-    x = x + random(-10,10)
-    y = y - 10
-   
-
-
-    if (y <0){
-        y = height;
-
-    }
-  
-
-   
     
+    for (var i = 0; i < peaches.length ; i++){
+        peaches[i].move();
+        peaches[i].display();
+    }
+   
+    for (var e = 0; e < peaches2.length;e++){
+        peaches2[e].move();
+        peaches2[e].display();
+    }
+    //image(crown,x1,y1,60,60);
+    //x1 = x1 + random(-15,15);
+    //y1 = y1 - 5;   
+    //if (y1 <0) {
+    //    y1 = height;
+    //}
+
+    fill(0);
+    textSize(56);
+    textAlign(CENTER, CENTER);
+    textStyle(BOLD);
+    text("hello world", 600, 600);
+    //x, y necessary
+    //fill first
+       
 }
 
 function mousePressed() {
-  
     var d = dist(mouseX, mouseY, 600, 600);
     if (d < 100) { 
-      r = random(255);
-      g = random(255);
-      b = random(255);
+      r = random(0,255);
+      g = random(0,255);
+      b = random(0,255);
     }
   }
+
+class Peach{
+    constructor(TempX,TempY,TempWidth,TempHeight,TempSpeed){
+        this.x = TempX
+        this.y = TempY
+        this.width = TempWidth
+        this.height = TempHeight
+        this.speed = TempSpeed
+    }
+    display(){
+        image(peach,this.x,this.y,this.width,this.height);
+
+
+    }
+    move(){
+        this.x = this.x + random(-10,10);
+        this.y = this.y - this.speed;
+        if (this.y <0){
+            this.y = height;
+        }
+
+    }
+
+}
+
 
